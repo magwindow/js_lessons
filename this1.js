4bf1b0adece8a182a216af8e15f5ddbf6e5b500a
magwindow <antosha.petrakov@yandex.ru>
2025-10-30 15:15:18 +0300

    this4

--- this1.js
+++ this1.js
@@ -29,11 +29,17 @@ person.loginfo.apply(bruce, ['developer', '123456789'])
 
 const array = [1,2,3,4,5]
 
-function multBy(arr, n) {
-    return arr.map(function(i) {
+//function multBy(arr, n) {
+//    return arr.map(function(i) {
+//        return i*n
+//    })
+//}
+
+Array.prototype.multBy = function(n){
+    return this.map(function(i) {
         return i*n
     })
 }
 
-console.log(multBy(array, 5))
+console.log(array.multBy(2))
 
