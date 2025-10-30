9edddb4745d6f669e355e9d033bc59573be8b4a9
magwindow <antosha.petrakov@yandex.ru>
2025-10-30 14:19:41 +0300

    prototype

--- prototype1.js
+++ prototype1.js
@@ -1,9 +1,16 @@
-const person = {
-    name: 'John',
-    age: 30,
-    greet: function () {
-        console.log(
-            `Hello, my name is ${this.name} and I am ${this.age} years old.`
-        )
-    }
+const person = new Object({
+        name: 'John',
+        age: 30,
+        greet: function () {
+            console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+        }
+    })
+
+Object.prototype.sayHello = function() {
+    console.log(`Hello!`)
 }
+
+const anonimous = Object.create(person)
+anonimous.name = 'Anonimous'
+
+const str = new String('I am a string')
