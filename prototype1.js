d792149ad4a04d9df19ad6e1aabd32194b5413f6
magwindow <antosha.petrakov@yandex.ru>
2025-10-30 13:58:11 +0300

    init commit

---
+++ prototype1.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'John',
+    age: 30,
+    greet: function () {
+        console.log(
+            `Hello, my name is ${this.name} and I am ${this.age} years old.`
+        )
+    }
+}
